289a6086b73c1e537b2326aaf85debc3b15e0893
Lucas Swiniarski <lucas.swiniarski@gmail.com>
2016-11-27 18:54:43 -0500

    new resnet

--- models/resnet.lua
+++ models/resnet.lua
@@ -24,6 +24,22 @@ function build_model(libs)
   end
 
   local model = nn.sequential()
+  model:add(ConvBN(3, 100, 7, 7, 1, 1, 1, 1)) --> 100 x 42 x 42
+
+  model:add(SpatialMaxPooling(2,2)) --> 100 x 21 x 21
+
+  local cat = nn.sequential()
+  cat:add(ConvBN(100, 150, 4, 4)) --> 150 x 18 x 18
+  cat:add(ConvBN(150, 250, 5, 5, 1, 1, 3, 3)) --> 250 x 18 x 18
+
+  model:add(Residual(cat))
+  model:add(SpatialMaxPooling(2,2)) --> 250 x 9 x 9
+  model:add(ConvBN(250, 250, 4, 4)) --> 250 x 6 x 6
+  model:add(SpatialMaxPooling(2, 2)) --> 250 x 3 x 3
+  model:add(nn.View(2250))
+  model:add(nn.Linear(2250, 300))
+  model:add(nn.ReLU())
+  model:add(nn.Linear(300, 43))
 
   return model
 end
