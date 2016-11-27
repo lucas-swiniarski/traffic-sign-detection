206a7c531209faa6ec154458c62cd93e2b03caa2
Lucas Swiniarski <lucas.swiniarski@gmail.com>
2016-11-27 15:28:55 -0500

    revert to first code

--- main.lua
+++ main.lua
@@ -1,133 +1,121 @@
 require 'torch'
 require 'optim'
 require 'os'
+require 'optim'
 require 'xlua'
-require 'math'
+-- require 'cunn'
+-- require 'cudnn' -- faster convolutions
+
+--[[
+--  Hint:  Plot as much as you can.  
+--  Look into torch wiki for packages that can help you plot.
+--]]
 
 local tnt = require 'torchnet'
 local image = require 'image'
 local optParser = require 'opts'
 local opt = optParser.parse(arg)
 
+local WIDTH, HEIGHT = 32, 32
 local DATA_PATH = (opt.data ~= '' and opt.data or './data/')
-
-local theta_max = opt.angle / 360 * math.pi
-
-local util = require 'utils'
 
 torch.setdefaulttensortype('torch.DoubleTensor')
 
+-- torch.setnumthreads(1)
 torch.manualSeed(opt.manualSeed)
+-- cutorch.manualSeedAll(opt.manualSeed)
 
-local lopt = opt
-local lfunctions = {}
-
-function getIterator(dataset, list_index_rebalanced, shuffle)
-
-  local d = nil
-
-  if list_index_rebalanced ~= nil then
-    d = tnt.BatchDataset{
-      dataset = tnt.ResampleDataset{
-        dataset = dataset,
-        size = table.getn(list_index_rebalanced),
-        sampler = function(dataset, idx)
-          return list_index_rebalanced[shuffle[idx]]
-        end
-      },
-      batchsize = opt.batchsize
-    }
-  else
-    d = tnt.BatchDataset{
-      dataset = dataset,
-      batchsize = opt.batchsize
-    }
-  end
-
-  return tnt.ParallelDatasetIterator{
-    nthread = opt.nThreads,
-    init = function()
-      local tnt = require 'torchnet'
-      local image = require'image'
-      local math = require 'math'
-
-      opt = lopt
-      theta_max = theta_max
-      DATA_PATH = DATA_PATH
-
-      list_index_rebalanced = list_index_rebalanced
-      shuffle = shuffle
-
-      local utils = require 'utils'
-    end,
-    closure = function()
-      return d
-    end
-  }
+function resize(img)
+    return image.scale(img, WIDTH,HEIGHT)
 end
 
-local trainData = torch.load(DATA_PATH ..'train.t7')
-local testData = torch.load(DATA_PATH ..'test.t7')
+--[[
+-- Hint:  Should we add some more transforms? shifting, scaling?
+-- Should all images be of size 32x32?  Are we losing 
+-- information by resizing bigger images to a smaller size?
+--]]
+function transformInput(inp)
+    f = tnt.transform.compose{
+        [1] = resize
+    }
+    return f(inp)
+end
+
+function getTrainSample(dataset, idx)
+    r = dataset[idx]
+    classId, track, file = r[9], r[1], r[2]
+    file = string.format("%05d/%05d_%05d.ppm", classId, track, file)
+    return transformInput(image.load(DATA_PATH .. '/train_images/'..file))
+end
+
+function getTrainLabel(dataset, idx)
+    return torch.LongTensor{dataset[idx][9] + 1}
+end
+
+function getTestSample(dataset, idx)
+    r = dataset[idx]
+    file = DATA_PATH .. "/test_images/" .. string.format("%05d.ppm", r[1])
+    return transformInput(image.load(file))
+end
+
+function getIterator(dataset)
+    --[[
+    -- Hint:  Use ParallelIterator for using multiple CPU cores
+    --]]
+    return tnt.DatasetIterator{
+        dataset = tnt.BatchDataset{
+            batchsize = opt.batchsize,
+            dataset = dataset
+        }
+    }
+end
+
+local trainData = torch.load(DATA_PATH..'train.t7')
+local testData = torch.load(DATA_PATH..'test.t7')
 
 trainDataset = tnt.SplitDataset{
-    partitions = {train=(100 - opt.val) / 100, val=opt.val / 100},
+    partitions = {train=0.9, val=0.1},
     initialpartition = 'train',
+    --[[
+    --  Hint:  Use a resampling strategy that keeps the 
+    --  class distribution even during initial training epochs 
+    --  and then slowly converges to the actual distribution 
+    --  in later stages of training.
+    --]]
     dataset = tnt.ShuffleDataset{
         dataset = tnt.ListDataset{
             list = torch.range(1, trainData:size(1)):long(),
             load = function(idx)
                 return {
-                    input =  getTrainSample(trainData, idx, DATA_PATH, theta_max),
-                    target = getTrainLabel(trainData, idx, DATA_PATH)
+                    input =  getTrainSample(trainData, idx),
+                    target = getTrainLabel(trainData, idx)
                 }
             end
         }
     }
 }
 
--- Shuffle at each epoch with fixed seed.
-function trainDataset:manualSeed(seed) torch.manualSeed(seed) end
-
 testDataset = tnt.ListDataset{
     list = torch.range(1, testData:size(1)):long(),
     load = function(idx)
         return {
-            input = getTestSample(testData, idx, DATA_PATH),
+            input = getTestSample(testData, idx),
             sampleId = torch.LongTensor{testData[idx][1]}
         }
     end
 }
 
--- If cudnn, get the fast convolutions
-libs = {}
 
-if opt.cudnn == true then
-    print("using cudnn")
-    require 'cudnn'
-    require 'cunn'
-    libs['SpatialConvolution'] = cudnn.SpatialConvolution
-    libs['SpatialMaxPooling'] = cudnn.SpatialMaxPooling
-    libs['ReLU'] = cudnn.ReLU
-else
-    libs['SpatialConvolution'] = nn.SpatialConvolution
-    libs['SpatialMaxPooling'] = nn.SpatialMaxPooling
-    libs['ReLU'] = nn.ReLU
-end
-
-require("models/" .. opt.model)
-
-local model = build_model(libs)
+--[[
+-- Hint:  Use :cuda to convert your model to use GPUs
+--]]
+local model = require("models/".. opt.model)
 local engine = tnt.OptimEngine()
 local meter = tnt.AverageValueMeter()
 local criterion = nn.CrossEntropyCriterion()
 local clerr = tnt.ClassErrorMeter{topk = {1}}
 local timer = tnt.TimeMeter()
 local batch = 1
-
-if opt.cudnn == true then
-  model = model:cuda()
-  criterion = criterion:cuda()
-end
 
 -- print(model)
 
@@ -143,55 +131,27 @@ engine.hooks.onStart = function(state)
     end
 end
 
-
--- Cuda for input / label
-engine.hooks.onSample = function(state)
-  if opt.cudnn == true then
-    state.sample.input = state.sample.input:cuda()
-    -- When Forwarding the testing set :
-    if state.sample.target ~= nil then
-      state.sample.target = state.sample.target:cuda()
-    end
-  end
-end
-
-local numberOfBatchs = 0
+--[[
+-- Hint:  Use onSample function to convert to 
+--        cuda tensor for using GPU
+--]]
+-- engine.hooks.onSample = function(state)
+-- end
 
 engine.hooks.onForwardCriterion = function(state)
     meter:add(state.criterion.output)
     clerr:add(state.network.output, state.sample.target)
     if opt.verbose == true then
         print(string.format("%s Batch: %d/%d; avg. loss: %2.4f; avg. error: %2.4f",
-                mode, batch, numberOfBatchs, meter:value(), clerr:value{k = 1}))
+                mode, batch, state.iterator.dataset:size(), meter:value(), clerr:value{k = 1}))
     else
-        xlua.progress(batch, numberOfBatchs)
+        xlua.progress(batch, state.iterator.dataset:size())
     end
     batch = batch + 1 -- batch increment has to happen here to work for train, val and test.
     timer:incUnit()
 end
 
--- After each epoch, if not learning : Divide by 2 learning rate.
-
-local best_val = nil
-
 engine.hooks.onEnd = function(state)
-
-    if mode == "Val" then
-
-      local val_err = clerr:value{k=1}
-
-      if best_val == nil then
-        best_val = val_err
-      end
-
-      if val_err > best_val then
-        opt.LR = opt.LR / 2
-        print("Not the best validation. Best so far : ", best_val, "New lr : ", opt.LR)
-      else
-        best_val = val_err
-      end
-    end
-
     print(string.format("%s: avg. loss: %2.4f; avg. error: %2.4f, time: %2.4f",
     mode, meter:value(), clerr:value{k = 1}, timer:value()))
 end
@@ -199,71 +159,50 @@ end
 local epoch = 1
 
 while epoch <= opt.nEpochs do
-  trainDataset:select('train')
+    trainDataset:select('train')
+    engine:train{
+        network = model,
+        criterion = criterion,
+        iterator = getIterator(trainDataset),
+        optimMethod = optim.sgd,
+        maxepoch = 1,
+        config = {
+            learningRate = opt.LR,
+            momentum = opt.momentum
+        }
+    }
 
-  -- list_index_rebalanced is a list of indexes for
-  list_index_rebalanced, shuffle = balanceTrainingSet(trainDataset, epoch, opt.nEpochs, trainData)
-
-  numberOfBatchs = torch.floor(table.getn(list_index_rebalanced) / opt.batchsize)
-
-  engine:train{
-      network = model,
-      criterion = criterion,
-      iterator = getIterator(trainDataset),
-      optimMethod = optim.sgd,
-      maxepoch = 1,
-      config = {
-          learningRate = opt.LR,
-          momentum = opt.momentum,
-          weightDecay = opt.weightDecay
-      }
-  }
-
-  trainDataset:select('val')
-  numberOfBatchs = torch.floor(trainDataset:size() / opt.batchsize)
-
-  engine:test{
-      network = model,
-      criterion = criterion,
-      iterator = getIterator(trainDataset)
-  }
-
-  print('Done with Epoch '..tostring(epoch))
-  epoch = epoch + 1
-
-  --trainDataset:select('train')
-  --trainDataset:exec('manualSeed', epoch)
-  --trainDataset:exec('resample')
+    trainDataset:select('val')
+    engine:test{
+        network = model,
+        criterion = criterion,
+        iterator = getIterator(trainDataset)
+    }
+    print('Done with Epoch '..tostring(epoch))
+    epoch = epoch + 1
 end
-
--- Do
--- Not
--- Change
--- Anything
--- Here
 
 local submission = assert(io.open(opt.logDir .. "/submission.csv", "w"))
 submission:write("Filename,ClassId\n")
 batch = 1
 
-numberOfBatchs = 100
 --[[
 --  This piece of code creates the submission
 --  file that has to be uploaded in kaggle.
 --]]
 engine.hooks.onForward = function(state)
-  local fileNames  = state.sample.sampleId
-  local _, pred = state.network.output:max(2)
-  pred = pred - 1
-  for i = 1, pred:size(1) do
-      submission:write(string.format("%05d,%d\n", fileNames[i][1], pred[i][1]))
-  end
-  xlua.progress(batch, numberOfBatchs)
-  batch = batch + 1
+    local fileNames  = state.sample.sampleId
+    local _, pred = state.network.output:max(2)
+    pred = pred - 1
+    for i = 1, pred:size(1) do
+        submission:write(string.format("%05d,%d\n", fileNames[i][1], pred[i][1]))
+    end
+    xlua.progress(batch, state.iterator.dataset:size())
+    batch = batch + 1
 end
 
 engine.hooks.onEnd = function(state)
-  submission:close()
+    submission:close()
 end
 
 engine:test{
--- opts.lua
+++ opts.lua
@@ -10,7 +10,7 @@ function M.parse(arg)
     cmd:option('-val',              10,             'Percentage to use for validation set')
     cmd:option('-nEpochs',          300,            'Maximum epochs')
     cmd:option('-batchsize',        128,            'Batch size for epochs')
-    cmd:option('-nThreads',         5,              'Number of dataloading threads')
+    cmd:option('-nThreads',         1,              'Number of dataloading threads')
     cmd:option('-manualSeed',       '0',            'Manual seed for RNG')
     cmd:option('-LR',               0.1,            'initial learning rate')
     cmd:option('-momentum',         0.9,            'momentum')
@@ -18,9 +18,10 @@ function M.parse(arg)
     cmd:option('-logDir',           'logs',         'log directory')
     cmd:option('-model',            '',             'Model to use for training')
     cmd:option('-verbose',          'false',        'Print stats for every batch')
-    cmd:option('-cudnn',            'false',             'Use cuda tensor')
-    cmd:option('-balance',            'false',             'Rebalance classes')
-    cmd:option('-angle',            '10',             'Rotate image randomly between - x and x degree')
+    --[[
+    -- Hint: Use this option to convert your code to use GPUs
+    --]]
+    -- cmd:option('-cuda',            '',             'Use cuda tensor')
 
     local opt = cmd:parse(arg or {})
 
