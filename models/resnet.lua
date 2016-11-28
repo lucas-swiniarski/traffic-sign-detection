933b04fcc03c162f7ea3367dcc33b73e9c6af9a1
Lucas Swiniarski <lucas.swiniarski@gmail.com>
2016-11-27 23:54:31 -0500

    New resnet

--- models/resnet.lua
+++ models/resnet.lua
@@ -31,8 +31,9 @@ function build_model(libs)
   model:add(SpatialMaxPooling(2,2)) --> 100 x 21 x 21
 
   local cat1 = nn.Sequential()
+  cat1:add(ConvBN(100, 100, 3, 5, 1, 1, 1, 1))
   cat1:add(ConvBN(100, 100, 5, 5, 1, 1, 2, 2))
-  cat1:add(ConvBN(100, 100, 5, 5, 1, 1, 2, 2))
+  cat1:add(ConvBN(100, 100, 7, 5, 1, 1, 3, 3))
 
   model:add(Residual(cat1)) --> 100 x 21 x 21
 
@@ -40,14 +41,22 @@ function build_model(libs)
   model:add(SpatialMaxPooling(2,2)) --> 150 x 9 x 9
 
   local cat2 = nn.Sequential()
+  cat2:add(ConvBN(150, 150, 3, 3, 1, 1, 1, 1))
   cat2:add(ConvBN(150, 150, 5, 5, 1, 1, 2, 2))
-  cat2:add(ConvBN(150, 150, 5, 5, 1, 1, 2, 2))
+  cat2:add(ConvBN(150, 150, 7, 7, 1, 1, 3, 3))
 
   model:add(Residual(cat2)) --> 150 x 9 x 9
 
   model:add(SpatialConvolution(150, 250, 4, 4)) -- 250 x 6 x 6
   model:add(ReLU())
   model:add(SpatialMaxPooling(2,2)) --> 250 x 3 x 3
+
+  local cat3 = nn.Sequential()
+  cat3:add(ConvBN(250, 250, 3, 3, 1, 1, 1, 1))
+  cat3:add(ConvBN(250, 250, 5, 5, 1, 1, 2, 2))
+  cat3:add(ConvBN(250, 250, 7, 7, 1, 1, 3, 3))
+
+  model:add(Residual(cat3))
 
   model:add(nn.View(2250))
   model:add(nn.Linear(2250, 300))
